ec35115a01c182ecb0dfe8f489882c3943a0acc1
loudjit <ernestilouis@gmail.com>
2023-06-20 15:20:01 +0200

    Affichage des sous-titres fonctionnel

--- app/src/main/java/com/limehug/speechlens/MainActivity.kt
+++ app/src/main/java/com/limehug/speechlens/MainActivity.kt
@@ -3,6 +3,7 @@ package com.limehug.speechlens
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.speech.RecognitionListener
@@ -10,14 +11,24 @@ import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.text.Html
 import android.util.Log
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.camera.core.ExperimentalGetImage
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -26,6 +37,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.translate.Translate
 import com.google.cloud.translate.TranslateOptions
 import com.google.cloud.translate.Translation
+import com.google.mlkit.vision.common.InputImage
 import com.limehug.speechlens.databinding.ActivityMainBinding
 import java.io.File
 import java.io.FileWriter
@@ -35,19 +47,83 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
-class MainActivity : AppCompatActivity() {
+@ExperimentalGetImage class MainActivity : AppCompatActivity() {
 
     private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
+    private var currentMode = "transcription"
+
 
     //camera
     private lateinit var binding : ActivityMainBinding
     private var imageCapture:ImageCapture?=null
+    private lateinit var textOverlayContainer: FrameLayout
+    private lateinit var textView: TextView
+
+
+    val options = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .build()
+    val faceDetector = FaceDetection.getClient(options)
+
+    private fun processImage(image: ImageProxy) {
+        val mediaImage = image.image
+        val rotation = image.imageInfo.rotationDegrees
+
+        mediaImage?.let {
+            val inputImage = InputImage.fromMediaImage(it, rotation)
+            faceDetector.process(inputImage)
+                .addOnSuccessListener { faces ->
+                    // Traitez les visages détectés
+                    processFaces(faces)
+                }
+                .addOnFailureListener { e ->
+                    // Gérez les erreurs de détection de visage
+                    Log.e(Constants.TAG, "Face detection failed: ${e.message}", e)
+                }
+                .addOnCompleteListener {
+                    // Fermez l'image Proxy après avoir terminé le traitement
+                    image.close()
+                }
+        } ?: image.close()
+    }
+
+    private fun processFaces(faces: List<Face>) {
+        // Supprimer tous les TextView précédemment ajoutés
+        textOverlayContainer.removeAllViews()
+
+        for (face in faces) {
+            val bounds = face.boundingBox // Récupérez les coordonnées du visage
+
+            // Calculer les coordonnées du TextView en dessous du visage
+            val textViewX = bounds.centerX().toFloat() + bounds.width().toFloat()
+            val textViewY = bounds.bottom.toFloat() + 6*bounds.height().toFloat()
+
+
+            if (textView.parent != null) {
+                (textView.parent as ViewGroup).removeView(textView)
+            }
+
+            // Créer et personnaliser votre TextView
+            textView.x = textViewX
+            textView.y = textViewY
+            textView.setTextColor(Color.WHITE)
+            textView.setBackgroundColor(Color.BLACK)
+            textView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            // Ajouter le TextView à votre conteneur textOverlayContainer
+            textOverlayContainer.addView(textView)
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        textOverlayContainer = findViewById(R.id.textOverlayContainer)
+        textView = findViewById(R.id.subtitleTextView)
 
 
         mediaRecorder = null
@@ -59,7 +135,7 @@ class MainActivity : AppCompatActivity() {
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
                 ActivityCompat.requestPermissions(this, permissions,0)
                 Toast.makeText(this, "Recording could not start!", Toast.LENGTH_SHORT).show()
 
@@ -76,15 +152,14 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        val buttonShow = findViewById<Button>(R.id.buttonShow)
-        buttonShow.setOnClickListener{
+        val buttonMode = findViewById<Button>(R.id.buttonMode)
+        buttonMode.setOnClickListener{
+            if (currentMode == "transcription") {
+                currentMode = "translation"
+            } else if (currentMode == "translation") { currentMode = "transcription"}
             showText()
         }
 
-        val buttonTranslate = findViewById<Button>(R.id.buttonTranslate)
-        buttonTranslate.setOnClickListener {
-            translateAudio()
-        }
 
         //CAMERA
         if(allPermissionsGranted()){
@@ -104,11 +179,21 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder().build().also{mPreview ->
                 mPreview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)}
 
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(ContextCompat.getMainExecutor(this)) { image ->
+                        // Traitez l'image
+                        processImage(image)
+                    }
+                }
+
             imageCapture = ImageCapture.Builder().build()
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
 
             try{cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture)
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture, imageAnalyzer)
 
             }catch(e: Exception){
                 Log.d(Constants.TAG,"startCamera fail:",e)
@@ -166,6 +251,7 @@ class MainActivity : AppCompatActivity() {
 
                 launch(Dispatchers.Main) {
                     Toast.makeText(this@MainActivity, "Audio translation completed!", Toast.LENGTH_SHORT).show()
+                    showText()
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -177,35 +263,34 @@ class MainActivity : AppCompatActivity() {
     private fun showText() {
         val filePath = getExternalFilesDir(null)?.absolutePath + "/recording.txt"
         val filePath2 = getExternalFilesDir(null)?.absolutePath + "/translation.txt"
-        val textView = findViewById<TextView>(R.id.recordingDisplay)
-        val textView2 = findViewById<TextView>(R.id.translationDisplay)
+        var textToShow = ""
 
         try {
-            val file = File(filePath)
-            if (file.exists()) {
-                val text = file.readText()
-                textView.text = text
-            } else {
-                textView.text = "File not found"
+            if (currentMode == "transcription") {
+                val file = File(filePath)
+                if (file.exists()) {
+                    val text = file.readText()
+                    textToShow = text
+                } else {
+                    textToShow = "File not found"
+                }
+            } else if (currentMode == "translation") {
+                val file2 = File(filePath2)
+                if (file2.exists()) {
+                    val text = file2.readText()
+                    textToShow = text
+                } else {
+                    textToShow = "File not found"
+                }
             }
         } catch (e: IOException) {
             e.printStackTrace()
-            textView.text = "Error reading file"
+            textToShow = "Error reading file"
         }
 
-        try {
-            val file2 = File(filePath2)
-            if (file2.exists()) {
-                val text = file2.readText()
-                textView2.text = text
-            } else {
-                textView2.text = "File not found"
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            textView2.text = "Error reading file"
-        }
+        textView.text = textToShow
     }
+
 
     //Gère la reconnaissance vocale (du bouton Record)
     private fun recognizeAudio() {
@@ -239,6 +324,8 @@ class MainActivity : AppCompatActivity() {
                     val recognizedText = recognizedTexts[0]
                     writeTextToFile(outputFile, recognizedText)
                     Toast.makeText(this@MainActivity, "Speech recognized and saved to file", Toast.LENGTH_SHORT).show()
+                    translateAudio()
+                    showText()
                 } else {
                     Toast.makeText(this@MainActivity, "No speech recognized", Toast.LENGTH_SHORT).show()
                 }
